4218a30ab41da3788067eeabef9c78386b247e31
Zac Abbott <zax.zm2@gmail.com>
2015-05-02 14:43:22 +0100

    stupid file not working
    it broke

--- class_tut.py
+++ class_tut.py
@@ -1,3 +1,27 @@
-class one(Object):
-	def __init__(x):
+
+class one():
+	
+	def __init__(self, x):
 		self.x = x
+
+
+
+	def add2(self):
+		return self.x + 2 
+
+class two(one):
+
+	def __init__(self, x, y):
+		super(two, self).__init__(x)
+		self.y = y
+		
+
+
+
+i1 = one(3)
+i2 = one(4)
+i3 = two(2, 2)
+
+print i1.add2()
+print i2.add2()
+print i3.add2()
